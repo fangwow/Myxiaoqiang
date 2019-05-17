b3df8b42d8cac6b375143a26b940d98da8a73637
songfang0 <2531599416@qq.com>
2019-05-17 14:20:24 +0800

    初始化项目

---
+++ src/components/home/header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="header">
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='scss'>
+    .header {
+        width: 100%;
+        height: 0.88rem;
+    }
+</style>
---
+++ src/components/home/home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="home">
+        <header-title></header-title>
+    </div>
+</template>
+
+<script>
+import headerTitle from '@/components/home/header.vue'
+export default {
+    data(){
+        return{
+
+        }
+    },
+    components: {
+        headerTitle
+    }
+}
+</script>
+
+<style>
+
+</style>
